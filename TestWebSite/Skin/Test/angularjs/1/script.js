b784943011d40908c4fded54b0d5e251d79616a0
mfc <914246767@qq.com>
2015-09-08 10:30:52 +0800

    测试脚本整理

---
+++ TestWebSite/Skin/Test/angularjs/1/script.js
@@ -0,0 +1,35 @@
+﻿function PhoneListCtrl($scope) {
+    $scope.phones = [
+    {
+        "name": "Nexus S",
+        "snippet": "Fast just got faster with Nexus S.",
+        "age": 0
+    },
+    {
+        "name": "Motorola XOOM™ with Wi-Fi",
+        "snippet": "The Next, Next Generation tablet.",
+        "age": 1
+    },
+    {
+        "name": "MOTOROLA XOOM™",
+        "snippet": "The Next, Next Generation tablet.",
+        "age": 2
+    }
+    ];
+
+    $scope.orderProp = 'age';
+    $scope.hello = "Hello, World!";
+}
+
+//function PhoneListCtrl($scope, $http) {
+//    $http.get('phones/phones.json').success(function (data) {
+//        $scope.phones = data;
+//    });
+
+//    $scope.orderProp = 'age';
+//}
+
+function InvoiceCntl($scope) {
+    $scope.qty = 1;
+    $scope.cost = 19.95;
+}
