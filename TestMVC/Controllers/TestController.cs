0446095d3288584d6cca5681c8624da8b16aaae0
mfc <392733084@qq.com>
2016-10-08 15:22:12 +0800

    创建mvc项目，并尝试简单的例子

---
+++ TestMVC/Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVC.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+        public ActionResult Index()
+        {
+            ViewBag.Message = "环境使用mvc";
+            return View();
+        }
+
+        public string GetString()
+        {
+            return "世界你好，我现在开始使用你了";
+        }
+
+        public ActionResult GetView()
+        {
+            return View("MyView");
+        }
+	}
+}
